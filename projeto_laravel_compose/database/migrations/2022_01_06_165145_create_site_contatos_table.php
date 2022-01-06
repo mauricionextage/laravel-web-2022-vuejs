9b8e2e6a93daed6d783426ceb2e00c8c2080ab43
mauricionextage <95771930+mauricionextage@users.noreply.github.com>
2022-01-06 14:16:56 -0300

    migrate

---
+++ projeto_laravel_compose/database/migrations/2022_01_06_165145_create_site_contatos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('telefone', 100);
+            $table->string('email', 100);
+            $table->integer('motivo_contato');
+            $table->text('menssagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_contatos');
+    }
+}
