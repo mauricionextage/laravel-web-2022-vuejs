b0af566efca2ebbea33ed4a387c1df85f3514667
mauricionextage <95771930+mauricionextage@users.noreply.github.com>
2022-01-06 10:27:50 -0300

    via get

--- projeto_laravel_compose/app/Http/Controllers/ContatoController.php
+++ projeto_laravel_compose/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
+        var_dump($_GET);
         $titulo = "ContatoS ";
         return view('site.contato', compact('titulo'));
     }
